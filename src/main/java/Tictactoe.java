c2170eaa542bce0e0e4b62c2230f6be27ef53c99
amankeshri05 <amankeshri2k20@gmail.com>
2026-05-01 19:10:30 +0530

    UC6:printing tictactoe and place will be move with State Update

--- src/main/java/Tictactoe.java
+++ src/main/java/Tictactoe.java
@@ -1,16 +1,45 @@
+import java.util.Scanner;
 public class Tictactoe {
-    static char[][] board = {
-            {'-', '-', '-'},
-            {'-', '-', '-'},
-            {'-', '-', '-'}
-    };
-    public static void main(String[] args) {
-        System.out.println(isValidMove(1, 1));
+    public static void displayBoard(char[][] board) {
+        System.out.println("\nCurrent Board:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j]);
+                if (j < 2) System.out.print(" | ");
+            }
+            System.out.println();
+            if (i < 2) System.out.println("--+---+--");
+        }
     }
-    static boolean isValidMove(int row, int col) {
-        return (row >= 0 && row < 3 &&
-                col >= 0 && col < 3 &&
-                board[row][col] == '-');
+    // UC6: Place Move on Board
+    public static boolean placeMove(char[][] board, int row, int col, char symbol) {
+        // Check valid move
+        if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
+            board[row][col] = symbol;  // State update
+            return true;
+        } else {
+            System.out.println("Invalid move! Try again.");
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[][] board = {
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+        };
+        char symbol = 'X'; // Example player
+        displayBoard(board);
+        System.out.print("\nEnter row (0-2): ");
+        int row = sc.nextInt();
+        System.out.print("Enter column (0-2): ");
+        int col = sc.nextInt();
+        if (placeMove(board, row, col, symbol)) {
+            System.out.println("\nMove placed successfully!");
+        }
+        displayBoard(board);
+        sc.close();
     }
 }
-
